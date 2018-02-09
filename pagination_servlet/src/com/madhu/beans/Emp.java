f8ae4e75024c302aba24687dbf13f21dc30c80f9
Madhuchandra07 <madhuan007@gmail.com>
2018-02-09 17:03:42 +0530

    hi madhu

---
+++ pagination_servlet/src/com/madhu/beans/Emp.java
@@ -0,0 +1,32 @@
+package com.madhu.beans;
+
+public class Emp {
+	private int id;
+	private String name;
+	private float salary;
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public float getSalary() {
+		return salary;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+
+}
